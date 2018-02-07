926526ba04fe3ec3c762a7034b8c8a8d8a9ea9c2
nannanoermark <36185803+nannanoermark@users.noreply.github.com>
2018-02-07 15:09:55 +0100

    Add files via upload

---
+++ mini_ex1/empty-example/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1800,1200);
+  background(153,3,88)
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  ellipse(100,50,100,100)
+  fill(20,75,200)
+
+  triangle(50,100,90,50,50,60)
+  fill(200,13,88)
+console.log("hello world");
+}
